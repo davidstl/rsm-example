53092087981c18f3d1298dd555e9935a0d051f4b
davidstl <davidstl@bitheads.com>
2018-07-05 13:51:23 -0400

    Listen HTTP

--- server/Scripts/main.js
+++ server/Scripts/main.js
@@ -17,12 +17,70 @@ var logger = log4js.getLogger('main');
 var ConnectionManager = require('./ConnectionManager.js');
 
 // Constants
-var PORT = 9306;
+var HTTP_PORT = 9306;
+var TCP_PORT = 9308;
+
+// Create the HTTP listener
+var express = require('express');
+var app = express()
+var qs = require('querystring');
+
+app.get('/', (request, res) =>
+{
+    logger.info("----------------------------------------------------\nbad request"); 
+    let query = require('url').parse(request.url, true).query;
+    logger.info("query: " + JSON.stringify(query));
+    res.writeHead(400, {'Content-Type': 'text/plain'});
+    res.write('bad request');
+    res.end();
+});
+
+function readPOSTData(request, callback)
+{
+    var body = '';
+
+    request.on('data', (data) =>
+    {
+        body += data;
+
+        // Too much POST data, kill the connection!
+        // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+        if (body.length > 1e6)
+        {
+            request.connection.destroy();
+        }
+    });
+
+    request.on('end', () =>
+    {
+        logger.info("----------------------------------------------------\nHeaders: " + JSON.stringify(request.headers));
+        logger.info("POST: " + body);
+
+        callback(body);
+    });
+}
+
+app.post('/bcrsm/room-assign', (request, res) => readPOSTData(request, data =>
+{
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('{"url":"http://207.219.200.99","port":"9206","roomId":"NO_ROOM_4_U"}');
+    res.end();
+}));
+
+app.post('/bcrsm/room-launch', (request, res) => readPOSTData(request, data =>
+{
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('{}');
+    res.end();
+}));
+
+app.listen(HTTP_PORT);
+logger.info("HTTP server listning on port " + HTTP_PORT);
 
 // Create our TCP server
 var server = net.createServer();
-server.listen(PORT);
-logger.info("TCP server listning on port " + PORT);
+server.listen(TCP_PORT);
+logger.info("TCP server listning on port " + TCP_PORT);
 
 // Receive connections
 server.on('connection', function(socket)
